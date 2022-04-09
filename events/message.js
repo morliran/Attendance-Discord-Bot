b378f3aca8e017f7a2c3f05c9674455d3b0a91a4
Liran mor <50165091+morliran@users.noreply.github.com>
2022-04-09 16:56:52 +0300

    Update message.js

--- events/message.js
+++ events/message.js
@@ -94,8 +94,5 @@ module.exports = new Event("messageCreate", async (client, message) => {
       `You do not have the permission \`${command.permission}\` to run this command!`
     );
 
-  let theMessage = message;
-  message.delete();
-
-  command.run(theMessage, args, client);
+  command.run(message, args, client);
 });
