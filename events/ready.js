39fee95b77a9769226117d080f7a778a8e946313
Liran mor <50165091+morliran@users.noreply.github.com>
2022-04-09 07:52:31 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,7 @@ const Event = require("../structures/event.js");
 require("dotenv").config();
 
 module.exports = new Event("ready", (client) => {
+  console.log(client);
   console.log(`${client.user.username} is online`);
   client.user.setActivity(`Use => ${process.env.BOT_PREFIX}help`, {
     type: "WATCHING",
